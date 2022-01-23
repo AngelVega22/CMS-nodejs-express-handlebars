9ec4008fa6e935f5f7f31b04d2c2034ef957fc5f
Angel <avegasam@gmail.com>
2022-01-23 00:05:43 -0500

    update

--- database/database.sql
+++ database/database.sql
@@ -17,6 +17,26 @@
 CREATE DATABASE IF NOT EXISTS `proyectotec` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `proyectotec`;
 
+-- Volcando estructura para tabla proyectotec.producto
+CREATE TABLE IF NOT EXISTS `producto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_artista` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `url_img` varchar(255) NOT NULL,
+  `genero` varchar(255) NOT NULL,
+  `precio` float NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+-- Volcando datos para la tabla proyectotec.producto: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+INSERT INTO `producto` (`id`, `nombre_artista`, `descripcion`, `url_img`, `genero`, `precio`, `id_user`) VALUES
+	(6, 'Shakira', 'cantante pop', 'https://images.pexels.com/photos/2231751/pexels-photo-2231751.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 'pop', 808, 6),
+	(7, 'Bob Marley', 'musica reggae', 'https://images.pexels.com/photos/894156/pexels-photo-894156.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 'raggea', 900, 7),
+	(12, 'Simple plan', 'Banda rock punk', 'https://images.pexels.com/photos/4415532/pexels-photo-4415532.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 'punk', 1140, 6);
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+
 -- Volcando estructura para tabla proyectotec.sessions
 CREATE TABLE IF NOT EXISTS `sessions` (
   `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
@@ -25,10 +45,10 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla proyectotec.sessions: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla proyectotec.sessions: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
 INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
-	('v82eir5QOoWCdRsbdyW9tYaK0eTi8_IF', 1642899244, '{"cookie":{"originalMaxAge":null,"expires":null,"httpOnly":true,"path":"/"},"passport":{"user":6}}');
+	('v82eir5QOoWCdRsbdyW9tYaK0eTi8_IF', 1643000292, '{"cookie":{"originalMaxAge":null,"expires":null,"httpOnly":true,"path":"/"},"passport":{"user":6}}');
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 
 -- Volcando estructura para tabla proyectotec.user
@@ -38,18 +58,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `last_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `last_login` datetime DEFAULT NULL,
-  `is_active` tinyint(4) NOT NULL,
+  `is_active` tinyint(4) DEFAULT NULL,
   `date_joined` datetime DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `is_superuser` bit(1) DEFAULT NULL,
   `user_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla proyectotec.user: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla proyectotec.user: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `last_login`, `is_active`, `date_joined`, `password`, `is_superuser`, `user_name`) VALUES
-	(6, 'Angel', 'Vega', 'aveg@gmail.com', '2022-01-21 08:27:33', 1, '2022-01-21 08:27:33', '$2a$10$I7JZS.cexfDGI8XxYqAPI.t9XjFGacm.FqjdMGk7vjvVSoV3t.3EG', b'1', 'avega');
+	(6, 'Angel', 'Vega', 'aveg@gmail.com', '2022-01-21 08:27:33', 1, '2022-01-21 08:27:33', '$2a$10$I7JZS.cexfDGI8XxYqAPI.t9XjFGacm.FqjdMGk7vjvVSoV3t.3EG', b'1', 'avega'),
+	(7, 'Juan  ', 'Gomez    ', 'prueba@prueba.com    ', '2022-01-22 20:45:02', 1, '2022-01-22 20:45:02', '$2a$10$zJSZGlWEeyYUcyOvufBF6eJrhgxTS4cAYEzfsuHBP9hbw/ajKeGsG', b'1', 'JuanP   ');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
