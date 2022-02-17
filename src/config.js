d93ca6e577e4ea05456ef51c6e4e60118cea0772
Angel <avegasam@gmail.com>
2022-02-17 01:33:30 -0500

    up

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 module.exports = {
     database: {
-        // connectionLimit: 10,
-        // host: 'localhost',
-        // user: 'root',
-        // password: '',
-        // database: 'escuela',
-        // port: 3306,
+        connectionLimit: 10,
         host: 'localhost',
-        user: 'dolcesal_escuela',
-        password: 'Contrasena123!',
-        database: 'dolcesal_escuela'
+        user: 'root',
+        password: '',
+        database: 'escuela',
+        port: 3306,
+        // host: 'localhost',
+        // user: 'dolcesal_escuela',
+        // password: 'Contrasena123!',
+        // database: 'dolcesal_escuela'
     }
 }
